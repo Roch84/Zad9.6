9b8212b47f7801efd32289f495ca89f8d0ea7db5
Jacek Kotlarski <kotlarski.j@gmail.com>
2018-02-16 23:04:34 +0100

    Change js, index-Task9.6 complete

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var list = document.getElementById('list'),
+	add = document.getElementById('addElem');
+
+add.addEventListener('click', function(element) {
+	var element = document.createElement('li');
+	var values = document.getElementsByTagName('li');
+	var value = values.length;
+	console.log(values.length);
+	list.appendChild(element);
+	element.innerHTML = 'item ' + value;
+	if (value == 5) {
+		alert('!!Caution-Be aware of creating too many buttons.');
+	}
+});
